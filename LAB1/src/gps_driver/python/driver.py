123af7cea4c76675a34605da46485b6e2a287bf3
Ravina <lad.ra@northeastern.edu>
2023-02-06 22:39:56 -0500

    changes in gps_msg

--- LAB1/src/gps_driver/python/driver.py
+++ LAB1/src/gps_driver/python/driver.py
@@ -7,7 +7,6 @@ import sys
 from gps_driver.msg import gps_msg
 from gps_driver.msg import *
 
-msg = gps_msg()
 def driver():
     
     rospy.init_node('talker', anonymous=True)
@@ -15,11 +14,11 @@ def driver():
     
     args = rospy.myargv(argv = sys.argv)
     if len(args) != 2:
-        print("error")
+        print(error)
         sys.exit(1)
 
     connected_port = args[1]
-    
+    msg = gps_msg()
     serial_port = rospy.get_param('~port', connected_port)
     #serial_port = rospy.get_param('~port', '/dev/pts/4')
     serial_baud = rospy.get_param('~baudrate', 4800)
@@ -69,10 +68,10 @@ def driver():
                 latitude_converted, longitude_converted)
 	      
 	    
-            msg.header.stamp.secs = int(total_utc_secs)
-            msg.header.stamp.nsecs = int(str(total_utc_nsecs)[:5])
+            msg.Header.stamp.secs = int(total_utc_secs)
+            msg.Header.stamp.nsecs = int(str(total_utc_nsecs)[:5])
             #print(f'UTM_East, UTM_north, Zone, Letter: {utm_lat_long}')
-            msg.header.frame_id = "GPS1_Frame"
+            msg.Header.frame_id = "GPS1_Frame"
             msg.Latitude = latitude_converted
             msg.Longitude = longitude_converted
             msg.Hdop = hdop
