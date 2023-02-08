009777c552d843c207253c6541b6c14bf81c618c
Ravina <lad.ra@northeastern.edu>
2023-02-08 13:20:45 -0500

    final changes

--- LAB1/src/gps_driver/python/driver.py
+++ LAB1/src/gps_driver/python/driver.py
@@ -3,130 +3,100 @@
 
 import rospy
 import serial
-from std_msgs.msg import Float64
-from gps_driver.msg import gps_msg
-# Package - gps_driver; msg - gps_msg
 import utm
-import re
-import argparse
 import sys
+from gps_driver.msg import gps_msg
+from gps_driver.msg import *
 
-pattern = r"^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$"
-
-def convert_utc_to_string(utc):
-    utc_string = utc[:2]+ " Hours " + utc[2:4] + " Minutes " + utc[4:] + " Seconds"
-    return utc_string
-
-def convert_lat_to_degree(latitude):
-    degree = int(latitude[:2]) + (float(latitude[2:])/60)
-    return degree
-
-def convert_long_to_degree(longitude):
-    degree = int(longitude[:3]) + (float(longitude[3:])/60)
-    return degree
-
-def gpgga_isCorrect(values):
-    if re.match(pattern, values[1]) and re.match(pattern, values[2]) and re.match(pattern, values[4]):
-        if values[3]=="N" or values[3]=="S":
-            if values[5]=="E" or values[5]=="W":
-                return True
-    return False
-
-if __name__ == '__main__':
-    '''
-    parser = argparse.ArgumentParser()
-    parser.add_argument("serial_port", type=str, help="GPS puck - serial port path")
-    args = parser.parse_args()
-    serial_port_arg = args.serial_port
-    '''
+def driver():
+    
+    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('gps', gps_msg, queue_size=10)
+    
     args = rospy.myargv(argv = sys.argv)
     if len(args) != 2:
-        print(error)
+        print("error")
         sys.exit(1)
 
-    serial_port_arg = args[1]
+    connected_port = args[1]
+    
+    serial_port = rospy.get_param('~port', connected_port)
+    serial_baud = rospy.get_param('~baudrate', 4800)
+    
+    port = serial.Serial(serial_port, serial_baud, timeout = 3)
+    
+    rospy.loginfo("Publishing parsed GPS data")
 
-    SENSOR_NAME = "gps"
-    rospy.init_node('driver')
-    #port = rospy.get_param('~port','/dev/pts/1') # hardcode serial port
-    port = rospy.get_param('~port',serial_port_arg)
-    baud = rospy.get_param('~baudrate',4800)
-    #rate = rospy.Rate(5) # 5hz
+    while not rospy.is_shutdown():  
+        	
+        data = str(port.readline())
+        data = data.lstrip("b'\\r$")
+        data = data.lstrip("b'$")
+        data = data.rstrip("\\n'")
+        if data == '':
+            rospy.logwarn("No GPS data found")
+        else:
+            recievedData = data.split(',')
+            if recievedData[0] == "GPGGA":
+                utc = float(recievedData[1])
+                utc_hours = utc // 10000
+                utc_minutes = (utc-(utc_hours*10000))//100
+                utc_seconds = (utc - (utc_hours*10000) - (utc_minutes*100))
+                total_utc_secs = (utc_hours*3600 + utc_minutes*60 + utc_seconds)
+                total_utc_nsecs = int((total_utc_secs * (10**7)))
 
-    port = serial.Serial(port, baud, timeout=3.)# Create conection
-    rospy.logdebug("Using GPS sensor on port "+serial_port_arg+" at "+str(baud))
+                latitude = float(recievedData[2])
+                latitude_direction = recievedData[3]
+                latitude_correction = 1
+                if latitude_direction == "S":
+                    latitude_correction = -1
 
-    rospy.sleep(0.2) # 5Hz sampling rate
+                latitude_degree = int(latitude/100)
+                latitude_minutes = float(latitude) - (latitude_degree * 100)
+                latitude_converted = latitude_correction * \
+                    float(latitude_degree + latitude_minutes/60)
 
-    rospy.loginfo("Publishing GPS and UTM")
+                longitude = float(recievedData[4])
+                longitude_direction = recievedData[5]
+                longitude_correction = 1
+                if longitude_direction == "W":
+                    longitude_correction = -1
 
-    pub = rospy.Publisher('/gps', gps_msg, queue_size=5)
+                longitude_degree = int(longitude / 100)
+                longitude_minutes = float(longitude) - (longitude_degree * 100)
+                longitude_converted = longitude_correction * \
+                    float(longitude_degree + longitude_minutes/60)
+
+                hdop = float(recievedData[8])
+                altitude = float(recievedData[9])
+
+                utc_data = recievedData[1]
+                utc_data = utc_data[:2]+ " Hours " + utc_data[2:4] + " Minutes " + utc_data[4:] + " Seconds"
+                utm_lat_long = utm.from_latlon(
+                    latitude_converted, longitude_converted)
+            
+                    
+                msg = gps_msg()
+                msg.Header.stamp.secs = int(total_utc_secs)
+                msg.Header.stamp.nsecs = int(str(total_utc_nsecs)[:5])
+                #print(f'UTM_East, UTM_north, Zone, Letter: {utm_lat_long}')
+                msg.Header.frame_id = "GPS1_Frame"
+                msg.Latitude = latitude_converted
+                msg.Longitude = longitude_converted
+                msg.HDOP = hdop
+                msg.Altitude = altitude
+                msg.UTM_easting = utm_lat_long[0]
+                msg.UTM_northing = utm_lat_long[1]
+                msg.UTC = utc_data
+                msg.Zone = utm_lat_long[2]
+                msg.Letter = utm_lat_long[3]
+                rospy.loginfo(msg)
+                pub.publish(msg)
+   
+
+if __name__ == '__main__':
 
     try:
-        while not rospy.is_shutdown():
-            line = port.readline()
-            #print(line)
-            # Sample read line - b'$GPGSV,3,2,12,08,05,173,27,07,70,338,16,31,00,000,26,51,37,207,*7A\n'
-            line_str = str(line)
-            #print(line_str)
-            line_str = line_str.lstrip("b'\\r$")
-            line_str = line_str.lstrip("b'$")
-            line_str = line_str.rstrip("\\n'")
-            #print(line_str)
-            if line_str == '':
-                rospy.logwarn("GPS: No data")
-            else:
-                values = line_str.split(',')
-                if(values[0] == "GPGGA"):
-                    if gpgga_isCorrect(values):
-                        latitude = convert_lat_to_degree(values[2])
-                        latitude_dir = values[3]
-                        longitude = convert_long_to_degree(values[4])
-                        longitude_dir = values[5]
-                        if latitude_dir == 'S':
-                            latitude = -latitude
-                        if longitude_dir == 'W':
-                            longitude = -longitude
-                        altitude = (float(values[9]))
-                        hdop = (float(values[8]))
-                        utc = convert_utc_to_string(values[1])
-                        utc_float = float(values[1])
-                        utc_hrs = utc_float
-                        utc_mnts = utc_float - (utc_hrs * 10000)
-                        utc_secs = (utc_float - (utc_hrs * 10000)) - (utc_mnts * 100)
-                        utc_secs_t = utc_hrs * 3600 + utc_mnts * 60 + utc_secs
-                        utc_nsecs_t = int (utc_secs_t * (10 ** 7))
-                        utm_coordinates = utm.from_latlon(latitude, longitude)
-                        
-                        utm_easting = (utm_coordinates[0])
-                        utm_northing = (utm_coordinates[1])
-                        zone_num = utm_coordinates[2]
-                        zone_letter = utm_coordinates[3]
-                        
-                        msg = gps_msg()
-                        msg.Header.stamp.secs = int (str(utc_secs_t)[:5])
-                        msg.Header.stamp.nsecs = int (str(utc_nsecs_t)[:5])
-                        msg.Header.frame_id = "GPS1_Frame"
-                        msg.Latitude =  latitude
-                        msg.Longitude = longitude
-                        msg.Altitude = altitude
-                        msg.HDOP = hdop
-                        msg.UTM_easting = utm_easting
-                        msg.UTM_northing = utm_northing
-                        msg.UTC = utc
-                        msg.Zone = zone_num
-                        msg.Letter = zone_letter
-                        pub.publish(msg)
-            #rate.sleep()
-
-    except Exception as e:
-        rospy.loginfo("An exception of type ", type(e).__name__, " was raised.")
-    
+        driver()
     except rospy.ROSInterruptException:
-        port.close()
-
-    except serial.serialutil.SerialException:
-        rospy.loginfo("Shutting down GPS node...")
-
-    #except:
-    #    rospy.loginfo("Unknown error...")
+        pass
